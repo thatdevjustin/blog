ebf837a9a15dfb3df58feed7c5cf2168ee370689
Justin <justin@thatdevjustin.com>
2014-04-10 22:47:35 -0400

    Finished setting up the blogpost class

--- blogpost.php
+++ blogpost.php
@@ -8,16 +8,48 @@
 		public $datePosted;
 
 		function __construct($inId=null, $inTitle=null, $inPost=null, $inAuthorId=null, $inDatePosted=null) {
-			$this->id = $inId;
-			$this->title = $inTitle;
-			$this->post = $inPost;
-
-			$splitDate = explode("-", $inDatePosted);
-			$this->datePosted = $splitDate[1] . "/" . $splitDate[2] . "/" . $splitDate[0];
-
-			$query = $mysql_query("SELECT first_name, last_name FROM People WHERE id = " . $inAuthorId);
-			$row = mysql_fetch_assoc($query);
-			$this->author = $row["first_name"] . " " . $row["last_name"];
+			//Setting the Post info
+			if (!empty($inId)){
+				$this->id = $inId;
+			}
+			if (!empty($inTitle)){
+				$this->title = $inTitle;
+			}
+			if (!empty($inPost)){
+				$this->post = $inPost;
+			}
+			//Setting up the Date
+			if (!empty($inDatePosted))
+				$splitDate = explode("-", $inDatePosted);
+				$this->datePosted = $splitDate[1] . "/" . $splitDate[2] . "/" . $splitDate[0];
+			}
+			//Setting up the Author
+			if (!empty($inAuthorId))
+				$query = $mysql_query("SELECT first_name, last_name FROM People WHERE id = " . $inAuthorId);
+				$row = mysql_fetch_assoc($query);
+				$this->author = $row["first_name"] . " " . $row["last_name"];
+			}
+			//Setting up the Tags
+			$postTags = "No Tags";
+			if (!empty($inId)){
+				$query = $mysql_query("SELECT tags.* FROM post_tag LEFT JOIN (tags) ON (post_tag.tag_id = tags.id) WHERE post_tag.post_id = " . $inId);
+				$tagArray = array();
+				$tagIDArray = array();
+				while($row = mysql_fetch_assoc($query)) {
+					array_push($tagArray, $row["name"]);
+					array_push($tagIDArray, $row["id"]);
+				}
+				if (sizeof($tagArray) > 0) {
+					foreach ($tagArray as $tag) {
+						if($postTags == "No tags."){
+							$postTags = $tag;
+						}else {
+							$postTags = $postTags . ", " . $tag;
+						}
+					}
+				}
+			}
+			$this->tags = $postTags;
 		}
 	}
 ?>

ad1216254b1732a20bd719825825cfaa73b67d1d
Justin <justin@thatdevjustin.com>
2014-04-11 00:36:17 -0400

    Finished setting up the index file.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head></head>
+<body>
+	<div id="wrapper">
+		<div id="main">
+			<h1>Welcome to my blog</h1>
+			<div id="blogPost">
+				<?php
+					include 'include.php';
+
+					$blogPosts = GetBlogPosts();
+					foreach ($blogPosts as $post) {
+						echo "<div class='post'>";
+						echo "<h2>" . $post->title . "</h2>";
+						echo "<p>" . $post->post . "</p>"
+						echo "<span class='post_footer'>Posted By: " . $post->author . " Posted On: " . $post->datePosted . " Tags: " . $post->tags . "</span>";
+						echo "</div>";
+					}
+				?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
